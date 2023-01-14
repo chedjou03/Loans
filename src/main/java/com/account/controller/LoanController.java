7cdc8003e7f8657f71fa5f3e6d73ee7c02980ed6
Simplice chedjou <chedjou03@yahoo.fr>
2023-01-14 13:33:46 -0600

    add logic to generate and use correlationId or traceID

--- src/main/java/com/account/controller/LoanController.java
+++ src/main/java/com/account/controller/LoanController.java
@@ -6,11 +6,10 @@ import com.account.model.Customer;
 import com.account.model.Properties;
 import com.account.repository.LoanRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,8 +23,12 @@ public class LoanController {
     @Autowired
     LoansServiceConfig  loansServiceConfig;
 
+    private static final Logger logger = LoggerFactory.getLogger(LoanController.class);
+
     @PostMapping("/myLoan")
-    public ArrayList<Loan> getLoanDetail(@RequestBody Customer customer){
+    public ArrayList<Loan> getLoanDetail(@RequestHeader("ChedjouBank-correlation-id") String correlationId,@RequestBody Customer customer){
+
+        logger.debug("ChedjouBank-correlation-id : {}. ", correlationId);
         ArrayList<Loan> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
         if(loans!=null){
             return loans;
